6411e32809c64cf9fcd3aa2197ff2e77bc7164d4
Lu Zhang <luz20@pitt.edu>
2019-03-23 18:44:08 -0400

    Create 21_Merge_Two_Sorted_Lists.java
    added a while-loop solution

---
+++ LinkedList/21_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution1 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        while (l1!=null || l2!=null){
+            if (l1==null){
+                curr.next = new ListNode(l2.val);
+                l2 = l2.next;
+                curr = curr.next;
+                System.out.print(1);
+            }
+            else if (l2==null){
+                curr.next = new ListNode(l1.val);
+                l1 = l1.next;
+                curr = curr.next;
+                System.out.print(2);
+
+            }
+            else if (l1.val<=l2.val){
+                curr.next = new ListNode(l1.val);
+                l1 = l1.next;
+                curr = curr.next;
+                System.out.print(3);
+            }
+            else if (l1.val>l2.val){
+                curr.next = new ListNode(l2.val);
+                l2 = l2.next;
+                curr = curr.next;
+                System.out.print(4);
+
+            }
+        }
+        return head.next;
+    }
+}
+
